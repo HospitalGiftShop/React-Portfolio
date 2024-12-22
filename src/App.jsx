92d1daa42ff2bbb0ddf725f7045bce880271cbf2
HospitalGiftShop <tmeans01@gmail.com>
2024-12-22 10:46:29 -0600

    removed reference to portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-
+import { Outlet } from 'react-router-dom';
 import Header from './components/UI/Header.jsx';
 import Footer from './components/UI/Footer.jsx';
-import Container from './components/UI/Container.jsx';
 import './App.css';
-import { useState } from 'react';
+
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('About');
   return (
     <div>
-      <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <Container
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      <Header />
+      <main className="mx-3">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
