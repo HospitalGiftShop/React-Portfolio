a47eeccc4986e23972624a287053e554bc3f6643
HospitalGiftShop <tmeans01@gmail.com>
2024-12-22 13:27:16 -0600

    add additiona project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,21 @@
 import rain from './assets/rain.jpg'
+import hobby from './assets/hobbies.jpg'
 
 const projects = [
-    {
-      title: 'Weather Dashboard',
-      liveImage: rain,
-      liveLink: "https://weather-dashboard-drtq.onrender.com/",
-      gitHubImage: 'bi-linkedin',
-      gitHub: "https://github.com/HospitalGiftShop/Weather-Dashboard.git",
-    },
-  ];
+  {
+    title: 'Weather Dashboard',
+    liveImage: rain,
+    liveLink: "https://weather-dashboard-drtq.onrender.com/",
+    gitHubImage: 'bi-github',
+    gitHub: "https://github.com/HospitalGiftShop/Weather-Dashboard",
+  },
+  {
+    title: 'Hobby Hub',
+    liveImage: hobby,
+    liveLink: "https://hospitalgiftshop.github.io/Hobby-Hub-Project/",
+    gitHubImage: 'bi-github',
+    gitHub: "https://github.com/HospitalGiftShop/Hobby-Hub-Project",
+  },
+];
 
-  export default projects;
+export default projects;
