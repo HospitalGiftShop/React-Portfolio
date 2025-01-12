b768e9c8da308d30d7a1c2a3cb01674ad00fb0a4
HospitalGiftShop <tmeans01@gmail.com>
2025-01-12 14:30:45 -0600

    adjust styling

---
+++ src/components/UI/Projects.jsx
@@ -0,0 +1,48 @@
+
+import hobby from '../../assets/hobbyhub.png'
+import prework from '../../assets/prework.png'
+import weather from '../../assets/weather-dashboard.png'
+import employeeTracker from '../../assets/employeeTracker.png'
+import vehicleBuilder from '../../assets/vehicleBuilder.png'
+import readme from '../../assets/README.png'
+
+const projects = [
+  {
+    title: 'Weather Dashboard',
+    liveImage: weather,
+    liveLink: "https://weather-dashboard-drtq.onrender.com/",
+    gitHub: "https://github.com/HospitalGiftShop/Weather-Dashboard",
+  },
+  {
+    title: 'Hobby Hub',
+    liveImage: hobby,
+    liveLink: "https://hospitalgiftshop.github.io/Hobby-Hub-Project/",
+    gitHub: "https://github.com/HospitalGiftShop/Hobby-Hub-Project",
+  },
+  {
+    title: 'Prework Study Guide',
+    liveImage: prework,
+    liveLink: "https://hospitalgiftshop.github.io/prework-study-guide/",
+    gitHub: "https://github.com/HospitalGiftShop/prework-study-guide",
+  },
+  {
+    title: 'Employee Tracker',
+    liveImage: employeeTracker,
+    liveLink: "https://drive.google.com/file/d/1VAV6jr2FhGvxJDd1JGhKXLVmELvzV77t/view",
+    gitHub: "https://github.com/HospitalGiftShop/Employee-Tracker",
+  },
+  {
+    title: 'Vehicle Builder',
+    liveImage: vehicleBuilder,
+    liveLink: "https://drive.google.com/file/d/1xOepG3-qykH3d-ufmLGHrt_iERwK8wox/view",
+    gitHub: "https://github.com/HospitalGiftShop/Vehicle-Builder",
+  },
+  {
+    title: 'README Generator',
+    liveImage: readme,
+    liveLink: "https://drive.google.com/file/d/1D81oyPdXQQl0aJej4O7UXEb8RnmaWPGt/view",
+    gitHub: "https://github.com/HospitalGiftShop/Command-Line-README-Generator",
+  },
+];
+
+export default projects;
